b8ed6aff60de0d6a778e0326fb3422f37e329a70
AntonyNET <tserber_evil@mail.ru>
2023-08-16 10:25:27 +0500

    initial commit

---
+++ src/Api.Client/Clients/TemplateClient.cs
@@ -0,0 +1,45 @@
+﻿namespace Byndyusoft.Template.Api.Client.Clients
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Byndyusoft.ApiClient;
+    using Microsoft.Extensions.Options;
+    using OpenTracing;
+    using OpenTracing.Tag;
+    using Settings;
+    using Contracts.TemplateEntity;
+
+    /// <inheritdoc cref="ITemplateClient" />
+    public class TemplateClient : BaseClient, ITemplateClient
+    {
+        private const string ApiPrefix = "api/v1/templates";
+        private readonly ITracer _tracer;
+
+        public TemplateClient(
+            HttpClient httpClient,
+            ITracer tracer,
+            IOptions<TemplateApiSettings> apiSettings
+        ) : base(httpClient, apiSettings)
+        {
+            _tracer = tracer;
+        }
+
+        public async Task<TemplateDto> GetTemplate(int templateId)
+        {
+            using var scope = _tracer.BuildSpan(nameof(GetTemplate)).StartActive(true);
+
+            try
+            {
+                var templateDto = await GetAsync<TemplateDto>($"{ApiPrefix}/{templateId}");
+                return templateDto;
+            }
+            catch (Exception)
+            {
+                scope.Span.SetTag(Tags.Error, true);
+                throw;
+            }
+        }
+    }
+
+}
---
+++ src/Api.Contracts/TemplateEntity/ITemplateClient.cs
@@ -0,0 +1,16 @@
+﻿namespace Byndyusoft.Template.Api.Contracts.TemplateEntity
+{
+    using System.Threading.Tasks;
+
+    /// <summary>
+    ///     Интерфейс http клиента
+    /// </summary>
+    public interface ITemplateClient
+    {
+        /// <summary>
+        ///     Получение dto
+        /// </summary>
+        /// <param name="templateId">ИД сущности</param>
+        Task<TemplateDto> GetTemplate(int templateId);
+    }
+}
